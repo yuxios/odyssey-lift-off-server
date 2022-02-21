b47a3cd48891f2904d9eb80a9551e48e7e1878d9
Yudi <yudibakshi@outlook.com>
2022-02-21 18:10:47 +0530

    add PORT number

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url,port } = await server.listen();
+  const { url,port } = await server.listen({port: process.env.PORT || 4000});
   console.log(`
       🚀 Server is running!😎
       🔉 Listening on port ${port}
